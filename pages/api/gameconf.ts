c3e8db6127a2a8a83eb87e37ac2291d6a2b09e50
mpoapostolis <mpoapostolis@gmail.com>
2022-04-13 23:37:50 +0300

    fix empty

--- pages/api/gameconf.ts
+++ pages/api/gameconf.ts
@@ -6,7 +6,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const db = await myDb();
   const collection = await db.collection("scenes");
   const data = await collection.find({ type: req.query.type }).toArray();
-  if (!data) res.status(400).json({ msg: "not type found" });
+  if (!data || data.length === 0)
+    res.status(400).json({ msg: "not type found" });
 
   switch (req.query.type) {
     case "lexigram":
