d1e19576ff72b73ac897a29326ed8ed0b4579bcf
mpoapostolis <mpoapostolis@gmail.com>
2022-11-14 17:17:20 +0200

    4 images

--- components/Clock/index.tsx
+++ components/Clock/index.tsx
@@ -74,10 +74,10 @@ export function Clock() {
     setDrag(false);
   };
   return (
-    <MiniGameWrapper status="CLOCK">
+    <MiniGameWrapper status="RUNNING">
       <div onMouseUp={reset} className="grid grid-cols-[1fr_0.3fr] p-10 h-full">
-        <div className="flex flex-col justify-center items-center w-full h-full">
-          <div className=" gap-x-2 flex">
+        <div className="flex    flex-col justify-center items-center w-full h-full">
+          <div className="relative gap-x-2  flex">
             {emptyArr.map((_, i) => (
               <div
                 key={i}
@@ -89,35 +89,51 @@ export function Clock() {
           </div>
           <div className="divider"></div>
           <br />
-          <div
-            onMouseLeave={reset}
-            className="rounded-full border border-base-100 overflow-hidden   relative w-96 h-96"
-          >
-            {uniqCh.map((ch, idx) => {
-              const sin = Math.sin(idx * angle * 0.01745329252);
-              const cos = Math.cos(idx * angle * 0.01745329252);
-              const style = {
-                top: `calc(${sin * 50 + 40}% - ${sin * 32}px)`,
-                left: `calc(${cos * 50 + 39.5}% - ${cos * 32}px)`,
-                transform: `rotate(0deg)`,
-              };
-              return (
-                <button
-                  key={idx}
-                  {...bind(ch)}
-                  style={style}
-                  className={clsx(
-                    "absolute  bg-opacity-50 rounded-full text-5xl font-bold h-20 z-50 flex  items-center justify-center w-20",
-                    {
-                      "bg-green-700": selected.includes(ch),
-                      "bg-base-200": !selected.includes(ch),
-                    }
-                  )}
-                >
-                  {ch}
-                </button>
-              );
-            })}
+          <div className="relative w-full flex justify-center ">
+            <div className="absolute left-0 top-0 border  w-40 h-40 rounded-full flex items-center justify-center">
+              One
+            </div>
+
+            <div className="absolute right-0 top-0 border  w-40 h-40 rounded-full flex items-center justify-center">
+              One
+            </div>
+            <div className="absolute left-0 bottom-0 border  w-40 h-40 rounded-full flex items-center justify-center">
+              One
+            </div>
+            <div className="absolute right-0 bottom-0 border  w-40 h-40 rounded-full flex items-center justify-center">
+              One
+            </div>
+
+            <div
+              onMouseLeave={reset}
+              className="rounded-full border border-base-100 overflow-hidden   relative w-96 h-96"
+            >
+              {uniqCh.map((ch, idx) => {
+                const sin = Math.sin(idx * angle * 0.01745329252);
+                const cos = Math.cos(idx * angle * 0.01745329252);
+                const style = {
+                  top: `calc(${sin * 50 + 40}% - ${sin * 32}px)`,
+                  left: `calc(${cos * 50 + 39.5}% - ${cos * 32}px)`,
+                  transform: `rotate(0deg)`,
+                };
+                return (
+                  <button
+                    key={idx}
+                    {...bind(ch)}
+                    style={style}
+                    className={clsx(
+                      "absolute  bg-opacity-50 rounded-full text-5xl font-bold h-20 z-50 flex  items-center justify-center w-20",
+                      {
+                        "bg-green-700": selected.includes(ch),
+                        "bg-base-200": !selected.includes(ch),
+                      }
+                    )}
+                  >
+                    {ch}
+                  </button>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="h-full  flex">
